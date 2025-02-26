dbeeb12dd987a5628f61aa77dac098e05cbada9a
masum raza <masumrazait@gmail.com>
2025-02-26 19:33:49 +0530

    java program practices

--- src/main/java/feb24/ReverseString.java
+++ src/main/java/feb24/ReverseString.java
@@ -1,4 +1,13 @@
 package feb24;
 
 public class ReverseString {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        String reversed="";
+        int len=str.length();
+        for(int i=len-1;i>=0;i--){
+            reversed=reversed+str.charAt(i);
+        }
+        System.out.println(reversed);
+    }
 }
